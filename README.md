3273a9cd000a79930538841916ad44cffa5a8982
r0r0 <scramble@gmail.com>
2019-10-11 23:01:34 -0500

    Clean up

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # mocp
- a wrapper for MOC player
+a wrapper for MOC player
+
+### Debug
+~~~~
+$ export DEBUG=mocp
+~~~~
 
 Usage:
 
 ~~~~
-var mocp = require('mocp');
+var mocp = require('mocp')
 
-mocp.info(); // Print information about the currently playing file
-mocp.append('/file.flac'); // Append the files/directories/playlists passed in the command line to playlist
-mocp.playit('/yourfile.flac'); // Play files given on the command line without modifying the playlist
-mocp.playlist('/yourlist.m3u'); // Play playlist
-mocp.play(); // Start playing from the first item on the playlist
-mocp.stop(); // Stop playing
-mocp.clear(); // clear playlist
-mocp.next(); // Play the next song
-mocp.repeat(); // Toggle a control (repeat)
-mocp.suffle(); // Toggle a control (shuffle)
-mocp.seek(105); // Seek by N seconds (can be negative)
-mocp.previous(); // Play the previous song
-mocp.pause(); // Toggle between playing and paused
-mocp.volume(100); // Adjust the PCM volume
-mocp.volumeUp(); // Increase volume
-mocp.volumeDown(); // Decrease volume
+mocp.start()                   // starts mocp daemon (this must be initialized unless mocp is already running)
+
+
+mocp.append('/file.flac')      // Append the files/directories/playlists passed in the command line to playlist
+mocp.clear()                   // clear playlist
+mocp.info()                    // Print information about the currently playing file
+mocp.next()                    // Play the next song
+mocp.pause()                   // Toggle between playing and paused
+mocp.play()                    // Start playing from the first item on the playlist
+mocp.playit('/yourfile.flac')  // Play files given on the command line without modifying the playlist
+mocp.playlist('/yourlist.m3u') // Play playlist
+mocp.previous()                // Play the previous song
+mocp.repeat()                  // Toggle a control (repeat)
+mocp.seek(105)                 // Seek by N seconds (can be negative)
+mocp.stop()                    // Stop playing
+mocp.suffle()                  // Toggle a control (shuffle)
+mocp.volume(100)               // Adjust the PCM volume
 ~~~~
--- index.js
+++ index.js
@@ -1,135 +1,52 @@
-// Rowan Hamilton
-// index.js
-// 2017
+const spawn = require('child_process').spawn
+    , debug = require('debug')
 
-var spawn = require('child_process').spawn;
-var volumeSetting = 10;
+exec = (cmd, cb) => {
+  /*
+    Had to use stdbuf here because mocp returns a
+    segfault message that we can't get through stdout
+    without some kind of redirection
+  */
+  cmd = ['-o0', 'mocp'].concat(cmd)
+  mocp = spawn('stdbuf', cmd)
 
+  mocp.stdout.on('data', (data) => {
+    return cb(null, data.toString())
+  })
 
-var start_server = function(){
-    var exec = spawn('mocp', ['-S']);
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
+  mocp.stderr.on('data', (data) => {
+    return cb(data.toString())
+  })
 }
 
-start_server(); // Start mocp service
-
-
-function volume(inp){
-        if (inp){
-            volumeSetting += 5;
-        }
-        else{
-            volumeSetting -= 5;
-        }
-    command('-v ' + volumeSetting);
-    return 1;
+command = (cmd) => {
+  exec(cmd, (err, result) => {
+    if (!err) {
+      debug('mocp')(result)
+      return result
+    } else {
+      debug('mocp:error')(err)
+      return err
+    }
+  })
 }
 
-var command = function(command){
-    var exec = spawn('mocp', [command]);
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
-}
-
-var seek = function(command, seconds){
-    var exec = spawn('mocp', [command, seconds]);
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
-}
-
-var cmdfile = function(command, file){
-    var exec = spawn('mocp', [command, file]);
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
-}
-
-var playlist = function(command, file){
-    var exec = spawn('mocp', [command, file, '-p']);
-        exec.stdout.on('data', function(data) {
-            console.log(data);
-        });
-}
-
-var volume_level = function(level){
-    var exec = spawn('mocp', ['-v', level]);
-        exec.stdout.on('data', function(data) {
-            console.log(data.toString());
-        });
-}
-
-exports.version = function() {
-    command('-V');
-}
-
-exports.info = function() {
-    command('-i');
-}
-
-exports.append = function(file) {
-    cmdfile('-a', file);
-}
-
-exports.playit = function(file) {
-    cmdfile('-l', file);
-}
-
-exports.enqueue = function(file) {
-    cmdfile('-q', file);
-}
-
-exports.playlist = function(file) {
-    playlist('-a', file);
-}
-
-exports.clear = function() {
-    command('-c');
-}
-
-exports.play = function() {
-    command('-p');
-}
-
-exports.seek = function(seconds) {
-    seek('-k', seconds);
-}
-
-exports.stop = function() {
-    command('-s');
-}
-
-exports.suffle = function() {
-    command('-ts');
-}
-
-exports.repeat = function() {
-    command('-tr');
-}
-
-exports.next = function() {
-    command('-f');
-}
-
-exports.previous = function() {
-    command('-r');
-}
-
-exports.pause = function() {
-    command('-G');
-}
-
-exports.volume = function(level){
-    volume_level(level);
-}
-
-exports.volumeUp = function() {
-    volume(true);
-}
-
-exports.volumeDown = function() {
-    volume(false);
+module.exports = {
+  append     : (file) =>    {command(['-a', file])},
+  clear      : () =>        {command(['-c'])},
+  enqueue    : (file) =>    {command(['-q', file])},
+  info       : () =>        {command(['-i'])},
+  next       : () =>        {command(['-f'])},
+  pause      : () =>        {command(['-G'])},
+  play       : () =>        {command(['-p'])},
+  playit     : (file) =>    {command(['-l', file])},
+  playlist   : (file) =>    {command(['-a', file])},
+  previous   : () =>        {command(['-r'])},
+  repeat     : () =>        {command(['-tr'])},
+  start      : () =>        {command(['-S'])},
+  seek       : (seconds) => {command(['-k', seconds])},
+  stop       : () =>        {command(['-s'])},
+  suffle     : () =>        {command(['-ts'])},
+  version    : () =>        {command(['-V'])},
+  volume     : (level) =>   {command(['-v', level])}
 }
